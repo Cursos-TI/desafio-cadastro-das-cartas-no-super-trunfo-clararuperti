d0f7db906a229a8d96edfeec989bac18f77b4296
clararuperti <anaclararuperti@gmail.com>
2025-02-07 20:32:11 +0000

    Adicionei os cálculos de pib per capita e densidade demográfica

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,11 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    int populacao, pontosTuristicos;
-    float area, pib;
-
+    int populacao, pontosTuristicos, densidade;
+    float pib, pibPerCapita, area;
+    
     printf("Digite a quantidade de habitantes: \n");
     scanf("%d", &populacao);
     printf("Digite a área da cidade: \n");
@@ -19,9 +18,12 @@ int main() {
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &pib);
 
+    pibPerCapita= pib / populacao;
+    densidade= populacao / area;
+
     printf("População: %d - Área: %f \n", populacao, area);
     printf("Pontos Turísticos: %d - PIB: %f \n", pontosTuristicos, pib);
-    
+    printf("Densidade Demográfica: %d - PIB Per Capita: %f\n", densidade, pibPerCapita);
 
     return 0;
 }
